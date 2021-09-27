7ef182f7cc1c9cafb09561534d3efec1081807f4
Vikas Jayaram <vikas.ramasethu@gmail.com>
2021-09-28 09:30:10 +1000

    update default keys

--- index.js
+++ index.js
@@ -26,39 +26,29 @@ app.post('/token', async function (req, res) {
     const client_assertion = await generatePrivateKeyJWT(context.data);
     var options = {
         method: 'POST',
-        url: context.data.IDP_TOKEN_ENDPOINT,
+        url: context.data.SINGPASS_TOKEN_ENDPOINT,
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         data: qs.stringify({
             grant_type: 'authorization_code',
-            client_id: context.data.CLIENT_ID,
+            client_id: context.data.SINGPASS_CLIENT_ID,
             client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
             client_assertion: client_assertion,
             code: code,
             redirect_uri: redirect_uri
         })
     };
-    console.log(options.data);
     try {
         const response = await axios.request(options);
-        console.log(response.data);
         const { id_token } = response.data;
         const publicKey = await loadPublicKey(context.data);
         const { payload, protectedHeader } = await jwtVerify(id_token, publicKey, {
             issuer: context.data.ISSUER,
             audience: context.data.CLIENT_ID
         })
-
-        console.log(protectedHeader);
-        console.log(payload);
         response.data.payload = payload;
         return res.status(200).send(response.data);
     } catch (error) {
         if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
             return res.status(error.response.status).send(error.response.data);
         } else if (error.request) {
             // The request was made but no response was received
@@ -77,7 +67,7 @@ async function loadPrivateKey(config) {
         const response = await axios.get(config.RELYING_PARTY_JWKS_ENDPOINT);
         const { keys } = response.data;
         keys[0].d = config.RELYING_PARTY_PRIVATE_KEY;
-        return await parseJwk(keys[0], config.IDP_SIGNING_ALG);;
+        return await parseJwk(keys[0], config.SINGPASS_SIGNING_ALG);;
     } catch (e) {
         return e;
     }
@@ -85,9 +75,8 @@ async function loadPrivateKey(config) {
 
 async function loadPublicKey(config) {
     try {
-        const response = await axios.get(config.IDP_JWKS_ENDPOINT);
-        console.log(response.data.keys);
-        const publicKey = await parseJwk(response.keys[0], config.IDP_SIGNING_ALG);
+        const response = await axios.get(config.SINGPASS_JWKS_ENDPOINT);
+        const publicKey = await parseJwk(response.keys[0], config.SINGPASS_SIGNING_ALG);
         return publicKey;
     } catch (e) {
         return e;
@@ -98,11 +87,11 @@ async function generatePrivateKeyJWT(config) {
     //const privateKeyPEM = crypto.createPrivateKey(config.PRIVATE_KEY.replace(/\\n/gm, '\n'));
     const key = await loadPrivateKey(config);
     const jwt = await new SignJWT({})
-        .setProtectedHeader({ alg: config.IDP_SIGNING_ALG, kid: config.RELYING_PARTY_KID, typ: "JWT" })
+        .setProtectedHeader({ alg: config.SINGPASS_SIGNING_ALG, kid: config.RELYING_PARTY_KID, typ: "JWT" })
         .setIssuedAt()
-        .setIssuer(config.CLIENT_ID)
-        .setSubject(config.CLIENT_ID)
-        .setAudience(config.AUDIENCE)
+        .setIssuer(config.SINGPASS_CLIENT_ID)
+        .setSubject(config.SINGPASS_CLIENT_ID)
+        .setAudience(config.SINGPASS_AUDIENCE)
         .setExpirationTime('2m') //The expiration time on or after which the JWT MUST NOT be accepted by NDI for processing. Additionally, NDI will not accept tokens with an exp longer than 2 minutes since iat. https://tools.ietf.org/html/rfc7519#section-4.1.4
         .setJti(uuid.v4())
         .sign(key);
